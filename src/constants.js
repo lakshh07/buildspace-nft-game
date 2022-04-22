ac9638ce544b5b37ecf41c099596dffc677397f7
lakshh07 <lakshk886@gmail.com>
2022-04-22 10:55:39 +0530

    completed tutorial

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0xac3aC80C3f0e701458aE471c23e75d15Ff0f69d1";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
